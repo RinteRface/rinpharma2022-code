a0fd2f8f33c832fd0ee1a99a828ddd96f2b87ca6
DivadNojnarg <dgranjon@ymail.com>
2022-10-28 17:09:48 +0200

    don't use ragg for shiny (causing issue locally)

--- app.R
+++ app.R
@@ -4,4 +4,5 @@
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
+options(shiny.useragg = FALSE)
 rpharma2022test::run_app() # add parameters here (if any)
